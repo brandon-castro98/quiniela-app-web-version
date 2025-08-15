6136d692ac9862263780dd0eb6669cf85e6668e3
brandon-castro98 <jonathan_works98@outlook.com>
2025-08-15 00:36:15 -0600

    "Fixed navigation"

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5d732650781b27cf643ee802d4bcc026",
+"flutter_bootstrap.js": "4f77f2107e0c6d1c381814a77c24a623",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "1b762ff4a2720c4624043742917f4ced",
 "/": "1b762ff4a2720c4624043742917f4ced",
-"main.dart.js": "854eb37a8f07c85044c821e4bddf3f59",
+"main.dart.js": "fb1cfe15faa6b1136ef849de08f4c989",
 "manifest.json": "830d57d8849a5dae09f131c7df859ccf",
 "version.json": "ecf6d8f0726b5c5c1ae0e36515d69d0e"};
 // The application shell files that are downloaded before a service worker can
